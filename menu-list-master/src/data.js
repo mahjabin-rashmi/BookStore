28081d7c957603796c9e28379502296f9590a293
MK Rashmi <98965834+mahjabin-rashmi@users.noreply.github.com>
2022-12-11 13:59:45 +0600

    This is BookStore website.

---
+++ menu-list-master/src/data.js
@@ -0,0 +1,183 @@
+const menu = [
+
+    {
+        id: 1,
+        title: 'A Modern Introduction to Programming',
+        category: 'Programming',
+        price: 20.11,
+        img: './images/2.webp',
+        desc: `JavaScript lies at the heart of almost every modern web application. `,
+    },
+    {
+        id: 2,
+        title: 'The Four Agreements: A Practical Guide to Personal Freedom',
+        category: 'Development',
+        price: 7.99,
+        img: './images/7.jpeg',
+        desc: `In The Four Agreements, author don Miguel Ruiz reveals the self-limiting beliefs.`,
+    },
+
+    {
+        id: 3,
+        title: 'Life Of Pi ',
+        category: 'Adventure',
+        price: 13.45,
+        img: './images/14.jpeg',
+        desc: `After the sinking of a cargo ship, a solitary lifeboat remains bobbing on the wild blue Pacific.`,
+    },
+
+    {
+        id: 6,
+        title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits',
+        category: 'Development',
+        price: 11.98,
+        img: './images/6.webp',
+        desc: `No matter your goals, Atomic Habits offers a proven framework for improving--every day.`,
+    },
+
+    {
+        id: 17,
+        title: 'The Sandman Vol. 1: Preludes & Nocturnes',
+        category: 'Comics',
+        price: 30.23,
+        img: './images/17.jpeg',
+        desc: `An occultist attempting to capture the physical embodiment of Death to bargain for eternal life traps. `,
+    },
+
+    {
+        id: 14,
+        title: 'HTML and CSS: Design and Build',
+        category: 'Programming',
+        price: 15.79,
+        img: './images/1.jpg',
+        desc: `Every day, more and more people want to learn some HTML and CSS. This Book is one of the best. `,
+    },
+
+    {
+        id: 7,
+        title: 'SQL in 10 Minutes a Day, Sams Teach Yourself',
+        category: 'Programming',
+        price: 16.99,
+        img: './images/3.jpeg',
+        desc: `Expert trainer and popular author Ben Forta created this book for most of us who just need to learn SQL.`,
+    },
+    {
+        id: 4,
+        title: 'React Explained: Your Step-by-Step Guide to React',
+        category: 'Programming',
+        price: 24.47,
+        img: './images/4.png',
+        desc: `React Explained is a fun and enjoyable introduction to React, which is the Javascript framework. `,
+    },
+    {
+        id: 5,
+        title: 'C Programming Language',
+        category: 'Programming',
+        price: 18.39,
+        img: './images/5.jpeg',
+        desc: `The authors present the complete guide to ANSI standard C 
+        language programming.`,
+    },
+
+    {
+        id: 8,
+        title: 'The Power of Positive Thinking',
+        category: 'Development',
+        price: 12.99,
+        img: './images/8.jpeg',
+        desc: `Written with the sole objective of helping the reader achieve a happy, satisfying, and worthwhile life.`,
+    },
+    {
+        id: 9,
+        title: 'The Magic of Thinking Big: (Vermilion Life Essentials)',
+        category: 'Development',
+        price: 16.40,
+        img: './images/9.jpg',
+        desc: `More than 6 million readers around the world have improved their lives by reading The Magic of Thinking.`,
+    },
+
+    {
+        id: 10,
+        title: 'The Universe Has Your Back: Transform Fear to Faith',
+        category: 'Development',
+        price: 14.99,
+        img: './images/10.png',
+        desc: `Each story and lesson readers to release the happiness, security, and clear direction.`,
+    },
+
+    {
+        id: 11,
+        title: 'Don Quixote (Penguin Classics)',
+        category: 'Adventure',
+        price: 14.92,
+        img: './images/11.webp',
+        desc: `Don Quixote has become so entranced reading tales of chivalry that he decides to turn knight errant himself.`,
+    },
+
+    {
+        id: 12,
+        title: 'Treasure Island (Signet Classics)',
+        category: 'Adventure',
+        price: 4.95,
+        img: './images/12.jpg',
+        desc: `For sheer storytelling delight and pure adventure, Treasure Island has never been surpassed.`,
+    },
+
+    {
+        id: 13,
+        title: 'Jaws: A Novel',
+        category: 'Adventure',
+        price: 12.79,
+        img: './images/13.jpeg',
+        desc: `When Peter Benchley wrote Jaws, he meticulously researched all available data about shark behavior.`,
+    },
+
+
+
+    {
+        id: 15,
+        title: 'Children of the Comet',
+        category: 'Adventure',
+        price: 16.99,
+        img: './images/15.png',
+        desc: `Torris’s daily struggle for survival includes harvesting frozen air to keep breathing for food.`,
+    },
+
+    {
+        id: 16,
+        title: 'Watership Down: A Novel',
+        category: 'Adventure',
+        price: 13.92,
+        img: './images/16.webp',
+        desc: `Richard Adams’s Watership Down is a timeless classic and one of the most beloved novels of all time.`,
+    },
+
+    {
+        id: 18,
+        title: 'BONE ONE VOLUME ED: One Volume Edition: v.1',
+        category: 'Comics',
+        price: 16.99,
+        img: './images/18.webp',
+        desc: `They make their way into a deep, forested valley filled with wonderful and terrifying creatures.`,
+    },
+
+    {
+        id: 19,
+        title: 'Maus I: A Survivors Tale',
+        category: 'Comics',
+        price: 15.22,
+        img: './images/19.jpeg',
+        desc: `The most successful narrative ever done about the Holocaust and the first masterpiece in comic book history.`,
+    },
+
+    {
+        id: 20,
+        title: 'THE AMAZING SPIDER-MAN OMNIBUS',
+        category: 'Comics',
+        price: 23.94,
+        img: './images/20.jpeg',
+        desc: `Stan Lee and Steve Ditko created one of the most enduring icons in American popular media:the amazing Spider-Man!`,
+    },
+
+];
+export default menu;
